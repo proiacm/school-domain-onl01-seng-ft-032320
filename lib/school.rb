b495a06e6546492baddfaad16f7a530d226b2d9a
CiaraMaria Proia <proiacm@gmail.com>
2020-03-31 18:07:47 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+  attr_accessor :name, :grade, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    if roster.has_key?(grade)
+      roster[grade] << student
+    else
+      roster[grade] = []
+      roster[grade] << student
+    end
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def sort 
+    roster.map do |grade, student|
+      roster[grade] = student.sort
+    end
+    roster
+  end
+end
